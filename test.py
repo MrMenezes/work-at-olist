7b0212c67748ba79a73752ef91a66fd800394196
MITIKAS-NT\MITIKAS <sr.tama@outlook.com>
2018-04-05 16:30:48 -0300

    Applying PEP-8

--- test.py
+++ test.py
@@ -4,20 +4,24 @@ import requests
 import json
 import sys
 
+
 class TestFlaskApiUsingRequests(unittest.TestCase):
-    def test_hello_world(self):
-        response = requests.get('http://localhost:5000/my-resource/0')
-        self.assertEqual(response.json(), {'hello': 'world'})
+
+  def test_hello_world(self):
+    response = requests.get('http://localhost:5000/my-resource/0')
+    self.assertEqual(response.json(), {'hello': 'world'})
 
 
 class TestFlaskApi(unittest.TestCase):
-    def setUp(self):
-        self.app = app.app.test_client()
 
-    def test_hello_world(self):
-        response = self.app.get('/my-resource/0')
-        self.assertEqual(json.loads(response.get_data().decode(sys.getdefaultencoding())), {'hello': 'world'})
+  def setUp(self):
+    self.app = app.app.test_client()
+
+  def test_hello_world(self):
+    response = self.app.get('/my-resource/0')
+    self.assertEqual(json.loads(response.get_data().decode(
+        sys.getdefaultencoding())), {'hello': 'world'})
 
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
